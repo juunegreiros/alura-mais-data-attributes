fd555c49252ae0d1abd3fc1b4e113027748c7f00
Ju <ju_bsnegreiros@hotmail.com>
2019-09-27 12:59:35 -0300

    Adiciona tabs

---
+++ index.js
@@ -0,0 +1,37 @@
+(() => { 
+  const tabs = document.querySelectorAll('[data-tab]')
+
+
+  const inativaTabs = () => {
+    tabs.forEach(tab => tab.classList.remove('ativa'))
+  }
+
+  const ativaTab = tab => {
+    tab.classList.add('ativa')
+  }
+
+  const escondeConteudos = () => {
+    const conteudos = document.querySelectorAll('[data-content]')
+
+    conteudos.forEach(conteudo => conteudo.classList.add('hide'))
+  }
+
+  const mostraConteudo = conteudo => {
+    conteudo.classList.remove('hide')
+  }
+
+  tabs.forEach(
+    tab => addEventListener('click', e => {
+      const tabSelecionada = e.target
+      const tabNumero = tabSelecionada.dataset.tab
+      const conteudoCorrespondente = document.querySelector(`[data-content="${tabNumero}"]`)
+      
+      inativaTabs()
+      escondeConteudos()
+
+      ativaTab(tabSelecionada)
+      mostraConteudo(conteudoCorrespondente)
+    })
+  )
+
+})()
